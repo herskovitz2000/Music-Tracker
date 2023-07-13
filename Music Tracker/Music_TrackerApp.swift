5ea6136b66ed2a2debe61ed2c16d7056f5801ec6
Nathan Herskovitz <nathanherskovitz@gmail.com>
2023-07-12 20:44:53 -0400

    Initial Commit

---
+++ Music Tracker/Music_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_TrackerApp.swift
+//  Music Tracker
+//
+//  Created by Nathan Herskovitz on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
